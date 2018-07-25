020051a7de6262a1c6f982204b0be8d2c84e39c5
Vladimir Saponko <vis@anadeainc.com>
2018-07-25 17:57:49 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_110630) do
+ActiveRecord::Schema.define(version: 2018_07_25_140612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "delivery_notes", force: :cascade do |t|
+    t.string "customer"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "incomes", force: :cascade do |t|
+    t.bigint "item_id"
+    t.integer "quantity"
+    t.decimal "price"
+    t.bigint "receipt_note_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_incomes_on_item_id"
+    t.index ["receipt_note_id"], name: "index_incomes_on_receipt_note_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "outcomes", force: :cascade do |t|
+    t.bigint "item_id"
+    t.integer "quantity"
+    t.decimal "price"
+    t.bigint "delivery_note_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["delivery_note_id"], name: "index_outcomes_on_delivery_note_id"
+    t.index ["item_id"], name: "index_outcomes_on_item_id"
+  end
+
+  create_table "receipt_notes", force: :cascade do |t|
+    t.string "supplier"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "incomes", "items"
+  add_foreign_key "incomes", "receipt_notes"
+  add_foreign_key "outcomes", "delivery_notes"
+  add_foreign_key "outcomes", "items"
 end
