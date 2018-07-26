8003a49d0d9ab0506dd0bd8d528f680c88805cd6
Vladimir Saponko <vis@anadeainc.com>
2018-07-26 19:45:12 +0300

    Add outcome quantity validation

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -1,9 +1,22 @@
 class Outcome < ApplicationRecord
+  include Report
   belongs_to :item
   belongs_to :delivery_note
 
   validates :delivery_note, presence: true
+  validates :quantity, numericality: { greater_than: 0, less_than_or_equal_to: :available_quantity }
 
   delegate :name, to: :item, prefix: true
   delegate :date, to: :delivery_note, prefix: 'out'
+
+  private
+
+    def available_quantity
+      items = Report.calculate_for(DateTime.now).reduce({}) do |h,item|
+        h.merge!({ item.name => item.quantity }) do |k, old_val, new_val|
+          old_val + new_val
+        end
+      end
+      items[item_name]
+    end
 end
