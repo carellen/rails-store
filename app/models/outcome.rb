8c00e347db5d448397145e1bfe6edec4f3402440
Vladimir Saponko <vis@anadeainc.com>
2018-07-27 11:32:40 +0300

    Refactor outcome quantity validation

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -4,19 +4,15 @@ class Outcome < ApplicationRecord
   belongs_to :delivery_note
 
   validates :delivery_note, presence: true
-  validates :quantity, numericality: { greater_than: 0, less_than_or_equal_to: :available_quantity }
+  validates :quantity, numericality: { greater_than: 0,
+    less_than_or_equal_to: :avialble_quantity }
 
   delegate :name, to: :item, prefix: true
   delegate :date, to: :delivery_note, prefix: 'out'
 
   private
 
-    def available_quantity
-      items = Report.calculate_for(DateTime.now).reduce({}) do |h,item|
-        h.merge!({ item.name => item.quantity }) do |k, old_val, new_val|
-          old_val + new_val
-        end
-      end
-      items[item_name]
+    def avialble_quantity
+      Report.available_quantity_for(item_id)
     end
 end
