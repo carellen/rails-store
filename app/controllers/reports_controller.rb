34c6c6fd85b6a94e98f97a02f4d7033d2d62be65
Vladimir Saponko <vis@anadeainc.com>
2018-07-30 10:18:36 +0300

    Add date to report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,20 @@
 class ReportsController < ApplicationController
-  include Report
 
   def show
-    @report = Report.calculate_for(DateTime.now)
+    @date = parse_date
+    @report = Report.new(@date)
   end
 
   private
 
     def report_params
       params.require(:report).permit(:date)
+    end
+
+    def parse_date
+      report_params[:date] ? DateTime.parse(report_params[:date]) :
+      DateTime.new(report_params["date(1i)"].to_i, report_params["date(2i)"].to_i,
+                  report_params["date(3i)"].to_i, report_params["date(4i)"].to_i,
+                  report_params["date(5i)"].to_i)
     end
 end
